bd824a54ea3866e522b570fb8491bae836e2daae
brettkan <bkfighton@gmail.com>
2018-09-11 21:08:00 -0700

    Add reportview

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './App.css'
 
 class App extends Component {
     state = {
         users: [],
         reports: [],
+        currentReportIndex: null,
     }
 
     componentDidMount() {
@@ -32,7 +34,34 @@ class App extends Component {
             })
     }
 
+    onReportTitleClick(reportId) {
+        this.setState({
+            currentReportIndex: reportId,
+        })
+    }
+
     render() {
+        const currentReportView = this.state.currentReportIndex === null ? (
+            <div>
+                <h3>Reports:</h3>
+                <ul>
+                    {this.state.reports.map((report, index) =>
+                        report.attributes ?
+                            <li className="report-item"
+                                key={report.id}
+                                onClick={() => this.onReportTitleClick(index)}>
+                                    {report.attributes.title}
+                            </li> :
+                            null
+
+                    )}
+                </ul>
+            </div>
+        ) : <ReportView 
+                reportTitle={this.state.reports[this.state.currentReportIndex].attributes.title}
+                info={this.state.reports[this.state.currentReportIndex].attributes.vulnerability_information}
+            />
+
         return (
             <div className="app">
                 <h1>Hello World!</h1>
@@ -40,18 +69,28 @@ class App extends Component {
                 {this.state.users.map(user =>
                    <div key={user.id}>{user.username}</div>
                 )}
-                <h3>Reports:</h3>
-                <ul>
-                    {this.state.reports.map(report =>
-                        report.attributes ?
-                            <li className="report-item" key={report.id}>{report.attributes.title}</li> :
-                            null
 
-                    )}
-                </ul>
+
+
+                {currentReportView}
             </div>
         )
     }
 }
+
+var ReportView({reportTitle, info}) {
+    return (
+        <div>
+            <h3>{reportTitle}</h3>
+            <p>{info}</p>
+        </div>
+    )
+}
+
+ReportView.propTypes = {
+    reportTitle: PropTypes.string.isRequired,
+    info: PropTypes.string.isRequired,
+}
+
 
 export default App
